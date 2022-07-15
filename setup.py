f26be00c694aabf5a60ecb6ec8795bfdcaeeded9
Alex Flint <alex.flint@gmail.com>
2022-07-15 15:16:48 -0400

    add setup.py, setup.cfg

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='logicalinduction',
+    version='0.1',
+    license='MIT',
+    author="Kōshin Alex Flint",
+    author_email='alex.flint@gmail.com',
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    url='https://github.com/alexflint/logical-induction',
+    keywords='logical induction',
+    install_requires=[],
+)
