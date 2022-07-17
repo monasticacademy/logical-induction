75207be9e5b3a92dbc69d3cfcd467e0970aaab4f
Alex Flint <alex.flint@gmail.com>
2022-07-17 11:08:25 -0400

    convert enumerator_test to pytest

--- logicalinduction/enumerator_test.py
+++ logicalinduction/enumerator_test.py
@@ -1,33 +1,32 @@
 import itertools
-from nose.tools import assert_equal
 
 from .enumerator import integer_vectors, product
 
 
 def test_integer_vectors():
     it = integer_vectors(3)
-    assert_equal(next(it), (0, 0, 0))
-    assert_equal(next(it), (0, 0, 1))
-    assert_equal(next(it), (0, 1, 0))
-    assert_equal(next(it), (1, 0, 0))
-    assert_equal(next(it), (0, 0, 2))
-    assert_equal(next(it), (0, 1, 1))
-    assert_equal(next(it), (0, 2, 0))
-    assert_equal(next(it), (1, 0, 1))
-    assert_equal(next(it), (1, 1, 0))
-    assert_equal(next(it), (2, 0, 0))
-    assert_equal(next(it), (0, 0, 3))
+    assert next(it) == (0, 0, 0)
+    assert next(it) == (0, 0, 1)
+    assert next(it) == (0, 1, 0)
+    assert next(it) == (1, 0, 0)
+    assert next(it) == (0, 0, 2)
+    assert next(it) == (0, 1, 1)
+    assert next(it) == (0, 2, 0)
+    assert next(it) == (1, 0, 1)
+    assert next(it) == (1, 1, 0)
+    assert next(it) == (2, 0, 0)
+    assert next(it) == (0, 0, 3)
 
 
 def test_product():
     it = product(itertools.cycle('abcd'), 2)
-    assert_equal(''.join(next(it)), 'aa')
-    assert_equal(''.join(next(it)), 'ab')
-    assert_equal(''.join(next(it)), 'ba')
-    assert_equal(''.join(next(it)), 'ac')
-    assert_equal(''.join(next(it)), 'bb')
-    assert_equal(''.join(next(it)), 'ca')
-    assert_equal(''.join(next(it)), 'ad')
-    assert_equal(''.join(next(it)), 'bc')
-    assert_equal(''.join(next(it)), 'cb')
-    assert_equal(''.join(next(it)), 'da')
+    assert ''.join(next(it)) == 'aa'
+    assert ''.join(next(it)) == 'ab'
+    assert ''.join(next(it)) == 'ba'
+    assert ''.join(next(it)) == 'ac'
+    assert ''.join(next(it)) == 'bb'
+    assert ''.join(next(it)) == 'ca'
+    assert ''.join(next(it)) == 'ad'
+    assert ''.join(next(it)) == 'bc'
+    assert ''.join(next(it)) == 'cb'
+    assert ''.join(next(it)) == 'da'
